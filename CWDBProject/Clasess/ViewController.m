e1e6614f6a9d1c4ae933284c57ba5501ccccae29
ChavezChen <543438338@qq.com>
2017-12-25 22:41:14 +0800

    路径还原

--- CWDBProject/Clasess/ViewController.m
+++ CWDBProject/Clasess/ViewController.m
@@ -19,7 +19,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    [self testGroupInsert];
+//    [self testGroupInsert];
     
 //    [self testMultiThreadingSqliteMore1];
 //    [self testQuery];
